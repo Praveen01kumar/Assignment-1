8f92b187723493be37fa35cf0dd82ae1b3061a68
Praveen01kumar <94226548+Praveen01kumar@users.noreply.github.com>
2022-04-13 16:01:48 +0530

    sdf

--- src/app/Services/service.service.ts
+++ src/app/Services/service.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, Subject, tap } from 'rxjs';
 import { AuthResponce } from '../appinterface/auth-responce';
 import { config } from '../config';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
@@ -8,22 +8,25 @@ import { PorgotPasswordComponent } from '../Components/cradencial/porgot-passwor
 import { SignUpComponent } from '../Components/cradencial/sign-up/sign-up.component';
 import { LogInComponent } from '../Components/cradencial/log-in/log-in.component';
 import { UpdateUserComponent } from '../Components/cradencial/update-user/update-user.component';
+import { User } from '../appinterface/user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
+  user = new Subject<User>();
+
   signedin = new BehaviorSubject(false)
   logout = new BehaviorSubject(true)
-  constructor(private http: HttpClient, public dialog: MatDialog) { }
+  constructor(private http: HttpClient, public dialog: MatDialog ) {this.authoSignIn(); }
 
   SignUp(email: string, password: string) {
     return this.http.post<AuthResponce>(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${config.API_KEY}`, {
       email: email,
       password: password,
       returnSecureToken: true
-    });
+    }).pipe(tap(res => { return this.authentictedUser(res.email, res.localId, res.idToken, +res.expiresIn); }));
   }
 
   SigIn(email: string, password: string) {
@@ -31,7 +34,7 @@ export class ServiceService {
       email: email,
       password: password,
       returnSecureToken: true
-    });
+    }).pipe(tap(res => { return this.authentictedUser(res.email, res.localId, res.idToken, +res.expiresIn); }));
   }
   //dialog opening
   openDialogSignin() {
@@ -75,5 +78,19 @@ export class ServiceService {
 
   }
 
+  authoSignIn(){
+//     const userData =JSON.parse(localStorage.getItem('User_Data'));
+//     if(!userData){
+//       return;
+//     }
+// const loggedInUser = new User()
+  }
+  private authentictedUser(email: string, userid: string, token: string, expiresIn: number) {
+    const expirationGate = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userid, token, expirationGate);
+    console.log("user =>", user);
+    this.user.next(user);
+    localStorage.setItem("User_Data", JSON.stringify(user));
+  }
 
 }
